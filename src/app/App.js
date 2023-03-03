030d50702326bb6690687cd90240faeabc9d0ce8
Andrei Beliakov <andrei@imd.ru>
2023-03-03 16:09:38 +0300

    feat:made Header and Card base structure

---
+++ src/app/App.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import { Header } from "../components/Header/Header";
+import { Card } from "../components/Card/Card";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Card />
+    </>
+  );
+}
+
+export default App;
